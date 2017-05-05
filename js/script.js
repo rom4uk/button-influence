53145e73ef09e42b717d2ae1dd6c38256da0e4c6
rom4uk <rsulyma86@gmail.com>
2017-05-05 18:44:00 +0300

    add js file

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*
+window.onload = function(){
+	alert( 'Hellow World' );
+};
+
+
+var login = prompt("enter login", "");
+var password;
+if(login == "admin"){
+	password = prompt("enter password", "");
+	if(password == "admin"){
+		alert("Hi, BRO");
+	}
+	else if(password == null){
+		alert("login canceled");
+	}
+	else{
+		alert("password is wrong");
+	}
+}
+else if(login == null){
+	alert("Вхід відмінено");
+}
+else{
+	alert("user don't exist");
+}
+ */
